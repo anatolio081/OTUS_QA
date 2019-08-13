d978d092181767143c4a757f8432c75edd960281
Anatoliy Tereshenko <anatolio081@gmail.com>
2019-08-14 00:16:07 +0300

    some fixes for readme.txt added comments in generator .py

--- lesson4/generator.py
+++ lesson4/generator.py
@@ -1,5 +1,5 @@
 import csv
-
+#без множеств не смог сделать данные уникальными, так как такие значения как города, карта, вклад, ипотека - дублируются
 with open('data2.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     next(reader)
@@ -14,9 +14,11 @@ with open('data2.csv') as csvfile:
         card.add(row[2])
         deposit.add(row[3])
         mortgage.add(row[4])
+#описал генератор на 4х множествах
 g = ((na, ci, ca, de, mo) for na in name for ci in city for ca in card for de in deposit for mo in mortgage)
 i = 0
 f = open('result.txt', 'w')
+#построчно загоняю данные из множеств в строки с табуляциями и переходом на следующую, как на примере
 while True:
     f.write(next(g)[0] + next(g)[1] + "\t" + next(g)[2] + "\t" + next(g)[3] + "\t" + next(g)[4] + "\n")
     i += 1
---
+++ lesson4/readme.txt
@@ -0,0 +1,15 @@
+     Чтобы правильно запустить скрипт требуется
+1)Склонировать репозиторий из гитахаба:
+    git clone https://github.com/anatolio081/OTUS_QA.git
+2)Открыть директорию OTUS_QA/lesson4:
+    cd OTUS_QA/lesson4
+3)В терминале прописать параметры окружения:
+    python3 -m venv env
+4)В том же терминале активировать созданное окружение:
+    source env/bin/activate
+5)Обновить pip:
+    pip install -U pip
+6)Через обновленный pip установить зависимости:
+    pip install -r requirements.txt
+5)Запустить скрипт командой:
+    python3 generator.py
