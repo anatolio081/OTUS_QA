00b4824802a50c255469c3e81855645f36f23952
Anatolio <tereshchenko@restream.rt.ru>
2019-08-14 14:12:37 +0300

    fix generator.py

--- lesson4/generator.py
+++ lesson4/generator.py
@@ -1,5 +1,6 @@
 import csv
-#без множеств не смог сделать данные уникальными, так как такие значения как города, карта, вклад, ипотека - дублируются
+
+# без множеств не смог сделать данные уникальными, так как такие значения как города, карта, вклад, ипотека - дублируются
 with open('data2.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     next(reader)
@@ -14,13 +15,14 @@ with open('data2.csv') as csvfile:
         card.add(row[2])
         deposit.add(row[3])
         mortgage.add(row[4])
-#описал генератор на 4х множествах
+# описал генератор на 4х множествах
 g = ((na, ci, ca, de, mo) for na in name for ci in city for ca in card for de in deposit for mo in mortgage)
 i = 0
 f = open('result.txt', 'w')
-#построчно загоняю данные из множеств в строки с табуляциями и переходом на следующую, как на примере
+# построчно загоняю данные из множеств в строки с табуляциями и переходом на следующую, как на примере
 while True:
-    f.write(next(g)[0] + next(g)[1] + "\t" + next(g)[2] + "\t" + next(g)[3] + "\t" + next(g)[4] + "\n")
+    result_raw = next(g)
+    f.write(result_raw[0] + result_raw[1] + "\t" + result_raw[2] + "\t" + result_raw[3] + "\t" + result_raw[4] + "\n")
     i += 1
     if i == 100:
         break
